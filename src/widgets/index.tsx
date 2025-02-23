09f02cd89190b8d1ff53c7090ae356a98547f5d6
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-02-23 12:38:08 +0800

    Add Selected Text Dictionary widget and register it in the plugin

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -38,6 +38,16 @@ async function onActivate(plugin: ReactRNPlugin) {
   await plugin.app.registerWidget('sample_widget', WidgetLocation.RightSidebar, {
     dimensions: { height: 'auto', width: '100%' },
   });
+
+  await plugin.app.registerWidget(
+    'selected_text_dictionary',
+    WidgetLocation.SelectedTextMenu,
+    {
+      dimensions: { height: 'auto', width: '100%' },
+      widgetTabIcon: 'https://cdn-icons-png.flaticon.com/512/2069/2069571.png',
+      widgetTabTitle: 'Dictionary',
+    }
+  );
 }
 
 async function onDeactivate(_: ReactRNPlugin) {}
---
+++ src/widgets/selected_text_dictionary.tsx
@@ -0,0 +1,7 @@
+import { renderWidget } from '@remnote/plugin-sdk';
+
+function SelectedTextDictionary() {
+  return <div>Hello World!</div>;
+}
+
+renderWidget(SelectedTextDictionary);
